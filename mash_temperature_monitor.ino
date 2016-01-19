cfe9619e44a249aba7a11b22893156f4baef5d4c
Xavier Tô <xavier.to@shopify.com>
2016-01-18 20:46:40 -0500

    Initial commit. Simple Dallas 1-wire sensor to Serial

---
+++ mash_temperature_monitor.ino
@@ -0,0 +1,25 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 10
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin(); 
+}
+
+
+void loop(void)
+{
+  if (Serial.available() > 0) { 
+    Serial.read();   
+    sensors.requestTemperatures();
+    Serial.println(sensors.getTempCByIndex(0)); 
+    Serial.flush();
+  }
+}
